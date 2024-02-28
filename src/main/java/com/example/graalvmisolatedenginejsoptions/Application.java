e2640ab3e8eae1201995818fde6ac6bbfe7dbb2b
Daniel Kaminski <dkaminski@wescale.com>
2024-02-28 10:43:04 +0100

    :recycle: Refactor example Application to display non-functioning js options

--- src/main/java/com/example/graalvmisolatedenginejsoptions/Application.java
+++ src/main/java/com/example/graalvmisolatedenginejsoptions/Application.java
@@ -1,7 +1,11 @@
 package com.example.graalvmisolatedenginejsoptions;
 
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Engine;
+import org.graalvm.polyglot.HostAccess;
+import org.graalvm.polyglot.PolyglotException;
 import org.graalvm.polyglot.SandboxPolicy;
+import org.graalvm.polyglot.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -26,18 +30,50 @@ public class Application implements ApplicationRunner {
             .sandbox(SandboxPolicy.ISOLATED)
             .out(out)
             .err(err)
-            .option("engine.MaxIsolateMemory", "150MB")
-            .option("sandbox.MaxCPUTime", "50ms")
-            .option("js.console", "false"); // none of the "js.xyz" options are possible
+            .allowHostAccess(HostAccess.newBuilder()
+                .allowAccessAnnotatedBy(HostAccess.Export.class)
+                .allowAllImplementations(false)
+                .allowAllClassImplementations(false)
+                .allowMapAccess(true)
+                .allowMutableTargetMappings()
+                .methodScoping(true)
+                .build())
+            .option("sandbox.MaxStatements", "50000")
+            ;
+
+
+        final Engine engine = Engine.newBuilder("js")
+            .sandbox(SandboxPolicy.ISOLATED)
+            .err(err)
+            .out(out)
+            .option("engine.MaxIsolateMemory", "1GB")
+            .option("sandbox.MaxCPUTime", "150ms")
+            .option("js.console", "false")
+//            .option("js.graal-builtin", "false") // only in TRUSTED
+//            .option("js.print", "false") // only in TRUSTED
+//            .option("js.load", "false") // only in TRUSTED
+            .build();
+
+        builder.engine(engine);
 
         try (final Context context = builder.build()) {
 
-            context.eval("js", """
-                console.log('foobar');
+            final Value result = context.eval("js", """
+                print(Graal.versionECMAScript);
+                print(Graal.versionGraalVM);
+                print(Graal.isGraalRuntime());
+                load({name: 'index.js', script: '\\'loading this should not have been possible\\';'});
                 """);
+
+            System.out.println(result.as(Object.class));
+
+            System.out.println(out);
+            System.err.println(err);
 
         } catch (IllegalArgumentException illegalArgumentException) {
             System.err.println(illegalArgumentException);
+        } catch (PolyglotException polyglotException) {
+            System.err.println(polyglotException);
         }
     }
 }
